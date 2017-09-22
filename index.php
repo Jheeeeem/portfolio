20ae0bb0d10c725ec584966908c377e38a24f932
Jheeeeem <jeaguilar015@gmail.com>
2017-09-22 20:56:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Leoviguill Login</title>
+</head>
+<body>
+<form method="POST" action="home.php">
+		<span>Username: </span><input type="text" name="acc_name" placeholder="Username"><br/><br/>
+		<span>Password: </span><input type="password" name="acc_pass" placeholder="Password"><br/><br>
+		<input type="submit" name="acc_login" value="Login">&nbsp;&nbsp;
+		<input type="submit" name="gst_login" value="Guest Login">
+</body>
+</html>
